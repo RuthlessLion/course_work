9005583a7db3d212659b3b0735fe4d00b0272db3
RuthlessLion <windowtheworldofgames@gmail.com>
2017-03-17 20:42:29 +0700

    Add all project

---
+++ cmake-build-debug/CMakeFiles/course_work.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/course_work.dir/main.cpp.o"
+  "course_work.pdb"
+  "course_work"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/course_work.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
